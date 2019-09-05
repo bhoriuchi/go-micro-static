bf7b7c7b397def3ed21c58f1846860169e6297e6
Branden Horiuchi <Branden.Horiuchi@blackline.com>
2019-09-04 22:30:09 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # go-micro-static
-Serve static files from micro api server
+Serve static files from go-micro api server index. The plugin was written to work with single page applications.
+
+## Usage
+
+Register the plugin before building Micro
+
+```go
+package main
+
+import (
+	"github.com/micro/micro/plugin"
+	"github.com/bhoriuchi/go-micro-static"
+)
+
+func init() {
+	plugin.Register(static.NewPlugin())
+}
+```
+
+The static directory can be supplied by the `--static_dir` option. The default is `html`
+
+```sh
+micro --static_dir=dist
+```
+
+When using along side API or WEB services, supply the service names to omit from serving with the plugin
+
+```sh
+micro --static_service=foo --static_service=bar
+```
---
+++ static.go
@@ -0,0 +1,76 @@
+package static
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/micro/cli"
+	"github.com/micro/micro/plugin"
+)
+
+type static struct {
+	dir      string
+	services []string
+}
+
+func (s *static) Flags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringSliceFlag{
+			Name:   "static_service",
+			Usage:  "Service name to exclude from static route matching",
+			EnvVar: "STATIC_SERVICE",
+		},
+		cli.StringFlag{
+			Name:   "static_dir",
+			Usage:  "Directory to serve static from",
+			EnvVar: "STATIC_DIR",
+		},
+	}
+}
+
+func (s *static) Commands() []cli.Command {
+	return nil
+}
+
+func (s *static) Handler() plugin.Handler {
+	return func(h http.Handler) http.Handler {
+		fs := http.FileServer(http.Dir(s.dir))
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if _, err := os.Stat(s.dir + r.RequestURI); os.IsNotExist(err) {
+				for _, service := range s.services {
+					prefix := fmt.Sprintf("/%s", strings.Trim(service, "/"))
+					if strings.HasPrefix(r.RequestURI, prefix) {
+						h.ServeHTTP(w, r)
+						return
+					}
+				}
+
+				http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
+			} else {
+				fs.ServeHTTP(w, r)
+			}
+		})
+	}
+}
+
+func (s *static) Init(ctx *cli.Context) error {
+	s.services = ctx.StringSlice("static_service")
+	dir := ctx.String("static_dir")
+	if len(dir) == 0 {
+		dir = "html"
+	}
+	s.dir = dir
+	return nil
+}
+
+func (s *static) String() string {
+	return "static"
+}
+
+// NewPlugin returns new plugin
+func NewPlugin() plugin.Plugin {
+	return &static{}
+}
